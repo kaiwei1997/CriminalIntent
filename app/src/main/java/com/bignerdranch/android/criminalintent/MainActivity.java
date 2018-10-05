6fac5cbab8eabe091f0edc58bac3d8cba857c1c1
Josh Tan <kaiweitan128@gmail.com>
2018-10-05 11:59:23 +0800

    CSC2074

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
